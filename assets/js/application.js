8d826d2e7a6e15020fddd9538dad457be13d1fe1
prakhar <prakhar1989@gmail.com>
2012-01-30 17:55:37 +0530

    Added points left in bidding page

--- assets/js/application.js
+++ assets/js/application.js
@@ -1,4 +1,23 @@
 $(document).ready(function(){
   $(".sorted").tablesorter({sortList: [[ 1, 0 ]]});
+
+    var totalpoints = 2000;
+    $('#points').text(totalpoints);
+    function computeScore(){
+      var score = 0;
+      $('input[type=text]').each(function(){
+          if($(this).val()){
+            score += parseInt($(this).val());
+          }
+      });
+      return score;
+    }
+    $('input[type=text]').change(function(){
+        // if (totalpoints - computeScore() > -1){
+          $('#points').text(totalpoints - computeScore());
+        // } else {
+        //   $('#points').text('Points Over!'); 
+        //   }
+    });
 });
 
